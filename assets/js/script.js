23cef795214973c304833416daa3c31f9a1ed2f7
Chubbray <chubbray1@gmail.com>
2020-06-10 23:25:22 -0600

    added states to html and serch to js

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function mySearchFunction() {
+    var input, filter, ul, li, item, i, txtValue;
+    input = document.getElementById("input");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("states");
+    li = ul.getElementsByTagName("li");  for (i = 0; i < li.length; i++) {
+      item = li[i];
+      txtValue = item.textContent || item.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
